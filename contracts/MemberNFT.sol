0a1f66edc77e79de486f6d2679a3d048dc314398
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-11-26 13:57:01 +0900

    MemberNFT:import OZ, inherit, define constructor

--- contracts/MemberNFT.sol
+++ contracts/MemberNFT.sol
@@ -2,4 +2,61 @@
 
 pragma solidity ^0.8.9;
 
-contract MemberNFT {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MemberNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
+    constructor() ERC721("MemberNFT", "MEM") {}
+
+    /**
+     * @dev
+     * - override
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev
+     * - override
+     */
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    /**
+     * @dev
+     * - override
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev
+     * - override
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
