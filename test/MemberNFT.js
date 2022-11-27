9491391301c7f4a8bd49024b63d042b68feae3ab
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-11-27 09:45:28 +0900

    MemberNFT:implement test preparation

--- test/MemberNFT.js
+++ test/MemberNFT.js
@@ -3,20 +3,24 @@ const { ethers } = require("hardhat");
 const { experimentalAddHardhatNetworkMessageTraceHook } = require("hardhat/config");
 
 describe("MemberNFT contract", function() {
+    let MemberNFT;
+    let memberNFT;
+    const name = "MemberNFT";
+    const symbol = "MEM";
+    let owner;
+
+    beforeEach(async function() {
+        [owner] = await ethers.getSigners();
+        MemberNFT = await ethers.getContractFactory("MemberNFT");
+        memberNFT = await MemberNFT.deploy();
+        await memberNFT.deployed();        
+    });
+    
     it("token name and symbol should be set", async function() {
-        const name = "MemberNFT";
-        const symbol = "MEM";
-        const MemberNFT = await ethers.getContractFactory("MemberNFT");
-        const memberNFT = await MemberNFT.deploy();
-        await memberNFT.deployed();
         expect(await memberNFT.name()).to.equal(name);
         expect(await memberNFT.symbol()).to.equal(symbol);
     });
     it("deploy address should be owner", async function() {
-        const [owner] = await ethers.getSigners();
-        const MemberNFT = await ethers.getContractFactory("MemberNFT");
-        const memberNFT = await MemberNFT.deploy();
-        await memberNFT.deployed();
         expect(await memberNFT.owner()).to.equal(owner.address);
     });
 })
