bdd63f961599018d03273dc43a082f75c82dd855
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-11-28 14:40:21 +0900

    TokenBank:define constructor

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -36,4 +36,11 @@ contract TokenBank {
 
     /// @dev Token withdraw event
     event TokenWithdraw(address indexed from, uint256 amount);
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+        owner = msg.sender;
+        _balances[owner] = _totalSupply;
+    }
 }
