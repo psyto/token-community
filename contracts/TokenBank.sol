78a15d24725f368039bd7d948a00c2aa10bcf45b
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-11-28 12:19:02 +0900

    TokenBank:NAT spec

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -3,24 +3,24 @@
 pragma solidity ^0.8.9;
 
 contract TokenBank {
-    // Token name
+    /// @dev Token name
     string private _name;
 
-    // Token symbol
+    /// @dev Token symbol
     string private _symbol;
 
-    // Token total supply count
+    /// @dev Token total supply count
     uint256 constant _totalSupply = 1000;
 
-    // TokenBank total deposit
+    /// @dev TokenBank total deposit
     uint256 private _bankTotalDeposit;
 
-    // TokenBank owner
+    /// @dev TokenBank owner
     address public owner;
 
-    // Token balances of each account address
+    /// @dev Token balances of each account address
     mapping(address => uint256) private _balances;
 
-    // Tokan balances, which TokenBank reserves for each account address
+    /// @dev Tokan balances, which TokenBank reserves for each account address
     mapping(address => uint256) private _tokenBankBalances;
 }
