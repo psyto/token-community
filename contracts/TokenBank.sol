4dece1fa96af51bfd2a400bf1f3dc82dc1567c68
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-11-28 15:56:22 +0900

    TokenBank:implement transfer function

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -63,4 +63,24 @@ contract TokenBank {
     function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
     }
+
+    /// @dev transfer Token
+    function transfer(address to, uint256 amount) public {
+        address from = msg.sender;
+        _transfer(from, to, amount);
+    }
+
+    /// @dev implement transfer
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        require(to != address(0), "Zero address cannot be specified for 'to'!");
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "Insufficient balance!");
+        _balances[from] = fromBalance - amount;
+        _balances[to] += amount;
+        emit TokenTransfer(from, to, amount);
+    }
 }
