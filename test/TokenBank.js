8aee5237a699b93f258f2b71caa1fef5df7cc550
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-11-28 17:51:55 +0900

    TokenBank:implement token transfer

--- test/TokenBank.js
+++ test/TokenBank.js
@@ -9,9 +9,12 @@ describe("TokenBank contract", function() {
     const name = "Token";
     const symbol = "TBK";
     let owner;
+    let addr1;
+    let addr2;
+    const zeroAddress = "0x0000000000000000000000000000000000000000";
 
     beforeEach(async function() {
-        [owner, addr1] = await ethers.getSigners();
+        [owner, addr1, addr2] = await ethers.getSigners();
         TokenBank = await ethers.getContractFactory("TokenBank");
         tokenBank = await TokenBank.deploy(name, symbol);
         await tokenBank.deployed();        
@@ -29,5 +32,30 @@ describe("TokenBank contract", function() {
             expect(await tokenBank.totalSupply()).to.equal(ownerBalance);
         });
     });
-
+    describe("transaction between address", function () {
+        beforeEach(async function () {
+            await tokenBank.transfer(addr1.address, 500);
+        });
+        it("token should be transferred", async function () {
+            const startAddr1Balance = await tokenBank.balanceOf(addr1.address);
+            const startAddr2Balance = await tokenBank.balanceOf(addr2.address);
+            await tokenBank.connect(addr1).transfer(addr2.address, 100);
+            const endAddr1Balance = await tokenBank.balanceOf(addr1.address);
+            const endAddr2Balance = await tokenBank.balanceOf(addr2.address);
+            expect(endAddr1Balance).to.equal(startAddr1Balance.sub(100));
+            expect(endAddr2Balance).to.equal(startAddr2Balance.add(100));
+        });
+        it("transfer to zero address should be failed", async function () {
+            await expect(tokenBank.transfer(zeroAddress, 100))
+            .to.be.revertedWith("Zero address cannot be specified for 'to'!");
+        });
+        it("transfer with insufficient amount should be failed", async function () {
+            await expect(tokenBank.connect(addr1).transfer(addr2.address, 510))
+            .to.be.revertedWith("Insufficient balance!");
+        });
+        it("transfer should trigger event", async function () {
+            await expect(tokenBank.connect(addr1).transfer(addr2.address, 100))
+            .emit(tokenBank, "TokenTransfer").withArgs(addr1.address, addr2.address, 100);
+        });
+    });
 })
