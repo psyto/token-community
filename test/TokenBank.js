50106627fa7bea8ed438d2021208f12dee4b86a2
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-11-29 10:00:04 +0900

    TokenBank:implement token deposit function

--- test/TokenBank.js
+++ test/TokenBank.js
@@ -11,10 +11,11 @@ describe("TokenBank contract", function() {
     let owner;
     let addr1;
     let addr2;
+    let addr3;
     const zeroAddress = "0x0000000000000000000000000000000000000000";
 
     beforeEach(async function() {
-        [owner, addr1, addr2] = await ethers.getSigners();
+        [owner, addr1, addr2, addr3] = await ethers.getSigners();
         TokenBank = await ethers.getContractFactory("TokenBank");
         tokenBank = await TokenBank.deploy(name, symbol);
         await tokenBank.deployed();        
@@ -31,6 +32,9 @@ describe("TokenBank contract", function() {
             const ownerBalance = await tokenBank.balanceOf(owner.address);
             expect(await tokenBank.totalSupply()).to.equal(ownerBalance);
         });
+        it("TokenBank balance should be zero", async function () {
+            expect(await tokenBank.bankTotalDeposit()).to.equal(0);
+        })
     });
     describe("transaction between address", function () {
         beforeEach(async function () {
@@ -56,6 +60,34 @@ describe("TokenBank contract", function() {
         it("transfer should trigger event", async function () {
             await expect(tokenBank.connect(addr1).transfer(addr2.address, 100))
             .emit(tokenBank, "TokenTransfer").withArgs(addr1.address, addr2.address, 100);
+        });
+    });
+    describe("Bank transaction", function () {
+        beforeEach(async function () {
+            await tokenBank.transfer(addr1.address, 500);
+            await tokenBank.transfer(addr2.address, 200);
+            await tokenBank.transfer(addr3.address, 100);
+            await tokenBank.connect(addr1).deposit(100);
+            await tokenBank.connect(addr2).deposit(200);
+        });
+        it("token can be deposited", async function () {
+            const addr1Balance = await tokenBank.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(400);
+            const addr1BankBalance = await tokenBank.bankBalanceOf(addr1.address);
+            expect(addr1BankBalance).to.equal(100);
+        });
+        it("token can be transfered after deposit", async function () {
+            const startAddr1Balance = await tokenBank.balanceOf(addr1.address);
+            const startAddr2Balance = await tokenBank.balanceOf(addr2.address);
+            await tokenBank.connect(addr1).transfer(addr2.address, 100);
+            const endAddr1Balance = await tokenBank.balanceOf(addr1.address);
+            const endAddr2Balance = await tokenBank.balanceOf(addr2.address);
+            expect(endAddr1Balance).to.equal(startAddr1Balance.sub(100));
+            expect(endAddr2Balance).to.equal(startAddr2Balance.add(100));
+        });
+        it("deposit should trigger TokenDeposit event", async function () {
+            await expect(tokenBank.connect(addr1).deposit(100))
+            .emit(tokenBank, "TokenDeposit").withArgs(addr1.address, 100);
         });
     });
 })
