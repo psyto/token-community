9a704a68d2c660f8d2ad4533bb62a2ad356a8b10
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-11-30 11:16:54 +0900

    frontend:connectWallet

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -39,6 +39,20 @@ export default function Home() {
       }
     }
   }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+      });
+      console.log(`account: ${accounts[0]}`);
+      setAccount(accounts[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   useEffect(() => {
     checkMetaMaskInstalled();
     checkChainId();
@@ -67,7 +81,14 @@ export default function Home() {
           <polygon points="175 175, 350 175, 350 350, 175 350" stroke="black" fill="#ffff00" />
         </svg>
       </div>
-
+      <div className={'flex mt-1'}>
+        {account === '' ? (
+          <button className={'bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded hover:border-transparent hover:text-white hover:bg-blue-500 hover:cursor-pointer'}
+            onClick={connectWallet}>
+            Connect to MetaMask
+          </button>
+        ) : (<></>)}
+      </div>
     </div>
   )
 }
